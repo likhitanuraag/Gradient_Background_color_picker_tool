b956263a665b59fba2b9b3c9bb5590ecea083065
Likhit Anuraag <likhitanuraag@gmail.com>
2019-10-12 14:59:50 +1100

    final update
    adding gradient description

--- script.js
+++ script.js
@@ -3,20 +3,17 @@ var color2 = document.querySelector(".color2");
 var css = document.querySelector("h3");
 var body = document.getElementById("gradient");
 
-color1.addEventListener("input", function() {
+function setGradient() {
     body.style.background = 
         "linear-gradient(to right, "
             + color1.value 
             + ", " 
             + color2.value
-            + ")";
-})
+            + ")"; 
 
-color2.addEventListener("input", function() {
-    body.style.background = 
-        "linear-gradient(to right, "
-            + color1.value 
-            + ", " 
-            + color2.value
-            + ")";
-})
+    css.textContent = body.style.background + ";"        
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
